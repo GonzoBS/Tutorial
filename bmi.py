9ee35ec0b456a7bb62fe390b1da8b30b16e0be80
GonzoBS <social@daniel-klann.de>
2016-03-29 22:34:57 +0200

    BMI Rechner erweitert und Funktionen ertellt.

--- bmi.py
+++ bmi.py
@@ -1,21 +1,48 @@
-#BMI Rechner
+#!/usr/bin/python
+# BMI Rechner
 
-groesse = float(input("Größe in m (1.67): "))
-gewicht = float(input("Gewicht in kg: "))
 
-ergebnis = (gewicht)/(groesse*groesse)
+def eingabe():
+    groesse = float(input("Größe in m (1.67): "))
+    gewicht = float(input("Gewicht in kg: "))
+    return gewicht,groesse
 
-print("Dein BMI beträgt: ", ergebnis)
 
-if ergebnis < 18.50:
-    uebersetzung = "Untergewicht"
-elif ergebnis < 25.00:
-    uebersetzung = "Normalgewicht"
-elif ergebnis < 30.00:
-    uebersetzung = "Übergewicht"
-elif ergebnis < 40.00:
-    uebersetzung = "Adipositas"
-elif ergebnis >= 40.00:
-    uebersetzung = "Adipositas Grad III"
+def berechnung(ge, gr):
+    return ge / (gr * gr)
 
-print("Du hast ", uebersetzung)
+
+def output(erg, aus):
+    print("Dein BMI beträgt:", erg)
+    print("Ergebnis gerundet:", round(erg, 2))
+    print("Du hast", aus)
+
+
+def auswertung(erg):
+    if erg < 18.50:
+        uebersetzung = "Untergewicht"
+    elif erg < 25.00:
+        uebersetzung = "Normalgewicht"
+    elif erg < 30.00:
+        uebersetzung = "Übergewicht"
+    elif erg < 40.00:
+        uebersetzung = "Adipositas"
+    elif erg >= 40.00:
+        uebersetzung = "Adipositas Grad III"
+    else:
+        uebersetzung = "Fehler"
+    return uebersetzung
+
+beenden = True
+einga = []
+while beenden:
+    einga = eingabe()
+    berech = berechnung(einga[0], einga[1])
+    auswert = auswertung(berech)
+    output(berech, auswert)
+    abfrage = input("Weitere Berechnung durchführen? (j/n): ")
+    if abfrage == "j":
+        beenden = True
+    else:
+        beenden = False
+        print("Auf wiedersehen")
